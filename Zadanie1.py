9a64a480c438b807f6c79c5d2656c1a6752fd2fe
Kamil <kamil.kolodziejski44@gmail.com>
2022-03-11 23:22:28 +0100

    Problem A zrobiony, TODO: przetestować aglorytm i rozwiązać problem metodą konstruktywną

--- Zadanie1.py
+++ Zadanie1.py
@@ -4,17 +4,22 @@ import numpy as np
 
 class Population:
     def __init__(self, population_size, cooperation_graph):
+        """Tworzymy populacje jako liste objektow Group"""
         self.available_id = set(cooperation_graph.keys())
         self.list = [Group(cooperation_graph=cooperation_graph,
                            group_size=random.randint(1, len(self.available_id)))
                      for i in range(population_size)]
         self.cooperation_graph = cooperation_graph
+        self.population_size = population_size
+        self.best_group_weight = 0
 
     def evaluate(self):
+        """Ewaluacja wszystkich osobnikow w populacji"""
         eval_list = [group.weight for group in self.list]
         return eval_list
 
     def cross(self, parent1, parent2):
+        """Krzyzowanie dwoch osobnikow"""
         set1 = set(random.sample(parent1.members, int(parent1.group_size / 2)))
         set2 = set(random.sample(parent2.members, int(parent2.group_size / 2)))
         child = Group(self.cooperation_graph,
@@ -22,36 +27,66 @@ class Population:
         return child
 
     def cross_all(self):
+        """Skrzyzowanie wszystkich osobnikow"""
         random.shuffle(self.list)
         N = len(self.list)
-        childs = []
-        if N == 0:
-            for parent1, parent2 in self.list[:N/2], self.list[N/2:]:
-                childs.append(self.cross(parent1, parent2))
+        children = []
+        if N % 2 == 0:
+            for parent1, parent2 in zip(self.list[:int(N/2)], self.list[int(N/2):]):
+                children.append(self.cross(parent1, parent2))
         else:
-            for parent1, parent2 in self.list[:int((N-1)/2)], self.list[int((N-1)/2):-1]:
-                childs.append(self.cross(parent1, parent2))
-        return childs
+            for parent1, parent2 in zip(self.list[:int((N-1)/2)], self.list[int((N-1)/2):-1]):
+                children.append(self.cross(parent1, parent2))
+        return children
+
+    def update_population(self):
+        """Wykonujemy iteracje, czyli rozmnzazamy i ewaloujemy nasza populacje"""
+        kids = self.cross_all()
+        new_population = self.list + kids
+        new_population.sort(key=lambda x: x.weight, reverse=True)
+        self.list = new_population[:self.population_size]
+        self.best_group_weight = self.list[0].weight
+
 
 class Group:
+    """Klasa Group przechowuje liste id czonkow grupy araz posiada metody do
+    ewaluacji i mutacji grupy"""
+
     def __init__(self, cooperation_graph, group_size=None, members=None):
+        available_id = set(cooperation_graph.keys())
         if members is None:
-            available_id = set(cooperation_graph.keys())
             self.members = random.sample(available_id, group_size)
-            self.weight = self.evaluate(cooperation_graph)
             self.group_size = group_size
+            self.weight = self.evaluate(cooperation_graph)
         elif group_size is None:
             self.members = members
             self.group_size = len(members)
+            self.mutate(available_id)
             self.weight = self.evaluate(cooperation_graph)
 
     def evaluate(self, cooperation):
-        weight = len(self.members)
-        for member1 in self.members:
-            for member2 in self.members:
+        """Liczymy ile jest osob w grupie a potem odejmujemy punkty za kazda
+        osobe ktora wspolpracowala z kims z grupy"""
+        temp_members = self.members.copy()
+        for member1 in temp_members:
+            for member2 in temp_members:
                 if member1 != member2 and member1 in cooperation[member2]:
-                    weight -= 1
+                    self.members.remove(member1) # wypierdalamy ludzi jesli ze soba pracowali
+                    break
+        weight = len(self.members)
+        self.group_size = weight
         return weight
+
+    def mutate(self, available_id):
+        """Tworzymy losowe mutacje w grupie z prawdopodobienstwem p"""
+        p = 0.1
+        N = int(self.group_size * p)
+        indexes = random.sample(range(self.group_size), N)
+        new_gens = random.sample(available_id, N)
+        temp = list(self.members)
+        for x, y in zip(indexes, new_gens):
+            temp[x] = y
+        self.members = set(temp)
 
     def is_group_admissible(self):
         if self.group_size == self.weight:
--- main.py
+++ main.py
@@ -6,8 +6,23 @@ from Zadanie1 import *
 if __name__ == '__main__':
     graph = graph_from_edges('data/Arxiv_GR_collab_network.txt',
                              directed=True, drop_rows=4)
-    population = Population(5, graph)
-    # print([m.members for m in population.list])
-    print([m.members for m in population.cross_all()])
+    population = Population(20, graph)
 
+    best_weight = 0
+    flag = 0
+    while True:
+        population.update_population()
+        temp = population.best_group_weight
+        if flag == 5:
+            break
+        elif best_weight >= temp:
+            flag += 1
+        else:
+            best_weight = temp
+            flag = 0
+
+    print(f'Best group: {population.list[0]} \n'
+          f'Weight: {population.best_group_weight}\n'
+          f'Admissible: {population.list[0].is_group_admissible()}\n'
+          f'Size: {population.list[0].group_size}')
 
