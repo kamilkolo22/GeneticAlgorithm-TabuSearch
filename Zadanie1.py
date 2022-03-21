5b1eea07cb53e53a85a3e005425c17e9f19d48f7
Kamil <kamil.kolodziejski44@gmail.com>
2022-03-21 10:40:06 +0100

    problem B zoptymalizowany, TODO: sprawdzanie listy Tabu

--- Zadanie1.py
+++ Zadanie1.py
@@ -105,70 +105,100 @@ class Group:
 # Zadanie 1 czesc B
 class City:
     def __init__(self, graph, max_qsize):
-        self.vertexes = list(graph.keys())
-        self.general_graph = graph
-        self.current_cover = graph
-        self.number_vertexes = len(graph)
-        self.max_qsize = max_qsize
-        self.tabu_list = deque()
-        self.tabu_list.appendleft(set(graph.keys()))
+        self.general_graph = graph  # wyjściowy graf
+        self.vertexes = set(graph.keys())  # zbió wszystkich wierzcholków
+
+        # self.how_many_looking = {x: len(graph[x]) for x in graph}
+        # self.current_cover = set(graph.keys())
+        self.how_many_looking = None  # ile wiercholków z pokrycia 'patrzy' na dany wierzchołek
+        self.current_cover = None  # aktualne pokrycie wierzchołkowe
+
+        self.number_vertexes = len(graph)  # liczba wszystkich wierzchołków
+        self.max_qsize = max_qsize  # maksymalny rozmiar listy tabu
+        self.tabu_list = deque()  # lista tabu w postaci deque
+        # self.tabu_list.appendleft(set(graph.keys()))
 
     def start_searching(self, stop_time):
+        """Zaczynamy szukac rozwiazania, startujemy od randomowego pokrycia"""
+        self.random_cover()
         time_start = time.time()
         i = 0
         while True:
+            # wyszukujemy lepszego rozwiazania w poblizu
             self.get_better_neighbour()
             i += 1
-            if (time.time() - time_start) > stop_time:
+            if (time.time() - time_start) > stop_time: # warunek stopu
                 break
         print(f'Number of interations: {i}')
         return self.current_cover
 
-    def check_cover(self, graph):
-        seen_vertexes = set().union(*graph.values())
+    def check_cover(self, cover=None):
+        """Sprawdzamy czy dany zbiór jest rzeczywiście pokryciem wierzchołkowym"""
+        if cover is None:
+            cover = self.current_cover
+        seen_vertexes = set().union(*{x: y for x, y in self.general_graph.items() if x in cover}.values())
+        seen_vertexes = seen_vertexes.union(cover)
         return len(seen_vertexes) == self.number_vertexes
 
     def get_better_neighbour(self):
-        new_cover = None
+        """Szukamy lepszego rozwiązania w pobliżu które nie jest na liście tabu"""
         for i in range(self.number_vertexes):
-            v_from = random.sample(self.current_cover.keys(), 1)[0]
+            v_from = random.sample(self.current_cover, 1)[0]
             v_to = random.sample(self.general_graph[v_from], 1)[0]
             if self.check_move_possibility(v_from, v_to):
-                new_cover = self.move_vertex(self.current_cover, v_from, v_to)
+                self.move_vertex(self.current_cover, v_from, v_to)
                 break
         else:
             print('Nie znaleziono możliwego ruchu')
 
-        if new_cover is not None and len(new_cover) < len(self.current_cover):
-            self.current_cover = deepcopy(new_cover)
+        # Resize listy tabu
         if len(self.tabu_list) > self.max_qsize:
             self.tabu_list.pop()
-        self.tabu_list.appendleft(set(new_cover.keys()))
 
-    def move_vertex(self, graph, v_from, v_to):
-        new_graph = deepcopy(graph)
-        new_graph.pop(v_from)
-        new_graph[v_to] = self.general_graph[v_to]
-        return new_graph
+
+    def move_vertex(self, v_set, v_from, v_to):
+        """Usunięcie wierzchołka lub przeniesienie wierzcholka z listy pokrycia"""
+        new_cover = v_set.copy()
+        new_cover.remove(v_from)
+        for v in self.general_graph[v_from]:
+            self.how_many_looking[v] -= 1
+        new_cover.add(v_to)
+        for v in self.general_graph[v_to]:
+            self.how_many_looking[v] += 1
+        # Jeśli nowe rozwiązanie jest lepsze to nadpisujemy
+        if len(new_cover) < len(self.current_cover):
+            self.current_cover = new_cover
+        self.tabu_list.appendleft(new_cover)
 
     def check_move_possibility(self, v_from, v_to):
-        neighbours = set().union(*{x: y for x, y in self.general_graph.items() if x in self.general_graph[v_from]}.values())
-        for neighbour in neighbours:
-            if len(self.current_cover[neighbour]) <= 1:
+        """Sprawdzamy czy dany ruch jest dopuszczalny, czyli czy dane rozwiązanie
+        znajduje się na liście rozwiązań dopuszczalnych"""
+        # TODO: dodać sprawdzenie czy rozwiąznie jest na lisćie tabu!
+        for neighbour in self.general_graph[v_from]:
+            if self.how_many_looking[neighbour] <= 1:
                 if neighbour in self.general_graph[v_to]:
-                    new_cover = set(self.current_cover.keys())
-                    new_cover.remove(v_from)
-                    new_cover.add(v_to)
-                    if new_cover in self.tabu_list:
-                        return False
-                    else:
-                        return True
+                    return True
                 else:
                     return False
-        new_cover = set(self.current_cover.keys())
-        new_cover.remove(v_from)
-        new_cover.add(v_to)
-        if new_cover in self.tabu_list:
-            return False
-        else:
-            return True
+        return True
+
+    def random_cover(self):
+        """Losowanie zbioru i dsotosowanie go tak aby był pokryciem wierzchołkowym"""
+        cover = set(random.sample(self.general_graph.keys(),
+                                  int(self.number_vertexes/2)))
+        how_many_looking = {x: 0 for x in self.vertexes}
+        for vertex in cover:
+            how_many_looking[vertex] += 1
+            for neighbour in self.general_graph[vertex]:
+                how_many_looking[neighbour] += 1
+        # Zbiór jaki trzeba uzupełnić żeby to było pokrycie  wierzchołkowe
+        to_add = set(x for x in self.vertexes if how_many_looking[x] <= 0)
+        for vertex in to_add:
+            if how_many_looking[vertex] <= 0:
+                cover.add(vertex)
+                how_many_looking[vertex] += 1
+                for neighbour in self.general_graph[vertex]:
+                    how_many_looking[neighbour] += 1
+        self.how_many_looking = how_many_looking
+        self.current_cover = cover
+        self.tabu_list.appendleft(cover)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from time import time
 
 
 def start_problem_A():
+    # TODO: przenieść tę funkcję do wnętrza klasy
     graph = graph_from_edges('data/Arxiv_GR_collab_network.txt',
                              directed=True, drop_rows=4)
     time_start = time()
@@ -35,5 +36,11 @@ if __name__ == '__main__':
     graph = graph_from_edges('data/roadNet_USRoads.txt', drop_rows=16)
     # print(len(graph))
     #
-    city = City(graph, 100)
-    print(f'Problem solved, found cover with {len(city.start_searching(stop_time=30))} vertexes')
+    city = City(graph, 1000)
+    time_start = time()
+    # city.random_cover()
+    city.start_searching(30)
+    print(f'Cover size: {len(city.current_cover)}, time: {time() - time_start}')
+    print(f'Is cover good: {city.check_cover()}')
+
+    # print(f'Problem solved, found cover with {len(city.start_searching(stop_time=20))} vertexes')
